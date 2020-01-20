0e95c22c8def2ae39f2b29cfe13f0a49d9895392
Srinath Thota <ext-srinath.thota@here.com>
2020-01-20 18:32:54 +0530

    added info about actor context.self

--- README.md
+++ README.md
@@ -17,3 +17,31 @@ we cannot create actor with new keyword
 all actors should have unique name
 
 we create actor using actorOf(Props[ActorName],"unique-name-to-identify-actor")
+
+------------
+we can instantiate actor which has constructor arguments 
+class Person(name:String) extends Actor{
+override def receive:Receive={
+
+}
+}
+
+person=actorSystem.actorOf(Props[new Person("args")])
+
+-------------
+to create actor with arguments best practice is to create companion object
+
+object Person{
+def methName = Props(new Person("args"))
+}
+class Person(name:String){
+
+}
+---------------------------------------
+Actor capabilities
+
+we can also send messages with method
+ex: specialMessages(content:String)  // it should be immutable , it should be serializable
+
+-----------------------------------
+we have keyword context where we can use it as this in java (context.self-->this in java)
--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -14,12 +14,32 @@ object ActorQuickstart extends App{
         println(s"i have received message $message")
         counter= message.split(" ").length;
         println(s"count is $counter")
+
+      case SpecialMessage(contents)=>println(s"got message from case class $contents")
+      case number:Int => println(s"i got numnber $number")
       case msg => println("i cannot understand")
+
+
     }
   }
 
-  val wordCounter = actorSystem.actorOf(Props[WordCount],"wordcount")
+  val wordCounter = actorSystem.actorOf(Props[WordCount])
+  val wordCounter2 = actorSystem.actorOf(Props[WordCount])
 
   wordCounter ! "I am learning  Akka"
+  wordCounter2 ! "I want to learn"
+
+  case class SpecialMessage(contents:String)
+  class Person(name:String) extends Actor{
+    override def receive: Receive = {
+      case "Hi"=>println("Print something")
+    }
+  }
+
+  val personActor = actorSystem.actorOf(Props(new Person("sri")),"person");
+  personActor ! "Hi"
+
+  wordCounter2 ! SpecialMessage("some special message")
+  wordCounter2 ! 50
 }
 
