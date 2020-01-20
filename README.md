81bdfefd6aeabaa9c76e102d38149d8163e64957
Srinath Thota <ext-srinath.thota@here.com>
2020-01-20 15:49:44 +0530

    Created first actor wordcount

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # actorsystem
+
+val actorSystem=ActorSystem("somename");  // some name can be alphanumeric but cannot contain characters
+1)Actors are uniquely identified
+2)Messages are Asunchronous
+3)Each actor may respond differently
--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,10 +1,25 @@
 //#full-example
 package com.example
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
 
 object ActorQuickstart extends App{
   val actorSystem=ActorSystem("firstactor")
   println(actorSystem.name)
+
+  class WordCount extends Actor{
+    var counter=0;
+    override def receive: Receive ={
+      case message:String =>
+        println(s"i have received message $message")
+        counter= message.split(" ").length;
+        println(s"count is $counter")
+      case msg => println("i cannot understand")
+    }
+  }
+
+  val wordCounter = actorSystem.actorOf(Props[WordCount],"wordcount")
+
+  wordCounter ! "I am learning  Akka"
 }
 
